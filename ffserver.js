fbd6c05f3f640e2103368a40dc3df168860f2463
Raine <8600557+dittoslash@users.noreply.github.com>
2025-07-22 19:44:44 +0100

    Express setup & moved docs

--- ffserver.js
+++ ffserver.js
@@ -1,13 +1,43 @@
-const {createServer} = require('node:http');
+var createError = require('http-errors');
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
 
-const hostname = '0.0.0.0';
-const port = 80;
-const server = createServer((req, res) => {
-	res.statusCode = 200;
-	res.setHeader('Content-Type', 'text/plain');
-	res.end('Hello World :3');
+//const SECRETS = require('./_SECRETS');
+//var libusers = require('./lib/users');
+//libusers.init();
+
+var app = express();
+
+//I think there's things I want to do that handlebars cannot do
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'hbs');
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser(SECRETS.cookieSign));
+app.use(express.static(path.join(__dirname, 'public')));
+
+//app.use('/user', require('./routes/user'));
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+	next(createError(404));
 });
 
-server.listen(port, hostname, () => {
-	console.log(`Server running at http://${hostname}:${port}/`)
-})
+// error handler
+/*
+app.use(function(err, req, res, next) {
+	// set locals, only providing error in development
+	res.locals.message = err.message;
+	res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+	// render the error page
+	res.status(err.status || 500);
+	res.send('oopsies ' + res.locals.error);
+});
+*/
+
+module.exports = app;
