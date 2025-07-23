b7709c4b425385df9db086f6ffcf3974b78f0816
luminoushaus <marshmhamilton@gmail.com>
2025-07-23 15:37:18 +0100

    current early dev of the draft page, will probably have to separate the js better

--- Public/java scrump.js
+++ Public/java scrump.js
@@ -1,4 +1,4 @@
-window.onload = function () {
+/*window.onload =*/swag = function () {
 
 	const offScreenMenu = document.querySelector('.off-screen-menu');
 
@@ -9,3 +9,33 @@ window.onload = function () {
 
 	})
 }
+
+var firstNameList = [
+	'Beverly', 'Steven', 'Gregory', 'Bethanne', 'Dave', 'Susan'
+];
+
+var surnameList = [
+	'Hills', 'House', 'White', 'Brown', 'Jones', 'Two'
+];
+
+var newPlayer = "";
+
+function generate() {
+	let playerNames = []; //create PlayerNames array
+
+	for (let i = 0; i < 4; i ++) { //do 4 times:
+		//make name
+		let name = firstNameList[Math.floor( Math.random() * firstNameList.length )] + " " + surnameList[Math.floor( Math.random() * surnameList.length)];
+		//add name to array. this isn't used but we're keeping it for funsies
+		playerNames.push(name);
+		//add name to element. 'abcd'[i] turns whatever number we're on into a letter from a-d. it gets letter #i from that string
+		document.getElementById("name" + "abcd"[i]).innerText = name;
+	}
+}
+/*
+	console.log(playerNames);
+	document.getElementById("playersa").innerText = playerNames;
+	document.getElementById("playersb").innerText = playerNames;
+	document.getElementById("playersc").innerText = playerNames;
+	document.getElementById("playersd").innerText = playerNames;
+*/
