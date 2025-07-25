fe28e4066b3ec99c3ed402cd00bf99ccecfd8780
luminoushaus <marshmhamilton@gmail.com>
2025-07-25 14:53:37 +0100

    now with stats

--- Public/java scrump.js
+++ Public/java scrump.js
@@ -18,24 +18,35 @@ var surnameList = [
 	'Hills', 'House', 'White', 'Brown', 'Jones', 'Two'
 ];
 
+var speciesList = [
+	'Human', 'Orc', 'Fae', 'Gnome', 'Elf', 'Dwarf', 'Centaur'
+];
+
 var newPlayer = "";
 
 function generate() {
 	let playerNames = []; //create PlayerNames array
 
-	for (let i = 0; i < 4; i ++) { //do 4 times:
+	for (let playerLetter of "ABCD") {
 		//make name
 		let name = firstNameList[Math.floor( Math.random() * firstNameList.length )] + " " + surnameList[Math.floor( Math.random() * surnameList.length)];
 		//add name to array. this isn't used but we're keeping it for funsies
 		playerNames.push(name);
-		//add name to element. 'abcd'[i] turns whatever number we're on into a letter from a-d. it gets letter #i from that string
-		document.getElementById("name" + "abcd"[i]).innerText = name;
+		//add name to element.
+		document.querySelector(`#player${playerLetter}>.name`).innerText = name
+
+		for (let stat of ["str", "dex", "end", "int", "ego", "fte"]) {
+			document.querySelector(`#player${playerLetter}>.stats>.${stat}`).innerText = getRandomInt(1, 10);
+		}
 	}
+
+	
 }
 
-function random(min,max) {
- return Math.floor((Math.random())*(max-min+1))+min;
- document.getElementById("stats" + "abcd"[i]).innerText =
+function getRandomInt(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 /*
 	console.log(playerNames);
